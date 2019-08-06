4768f1303625fc6d0f3454de2dd098368394f8b8
Davis Whitehead <whitehead.davis@gmail.com>
2019-08-06 09:10:21 -0400

    re-enabling posts function to merge all new posts data

--- functions/src/lib/Atoms.ts
+++ functions/src/lib/Atoms.ts
@@ -1,13 +1,13 @@
 import * as _ from 'lodash';
 
 export const createPostAtom = (post: any, postId: any = false) => ({
-  ..._.omit(post, 'createdAt', 'updatedAt', 'id', 'ref'),
-  postId: postId || post.id,
+  ..._.omit(post, 'createdAt', 'updatedAt', '_id', '_reference'),
+  postId: postId || post._id
 });
 
 export const createUserAtom = (user: any, userId: any = false) => ({
   userId: userId || user.id,
   userFirstName: user.firstName,
   userLastName: user.lastName,
-  userFacebookProfilePhoto: user.facebookProfilePhoto,
+  userFacebookProfilePhoto: user.facebookProfilePhoto
 });
--- functions/src/posts/Post.ts
+++ functions/src/posts/Post.ts
@@ -1,26 +1,21 @@
 import * as _ from 'lodash';
-import {
-  getDocumentsInCollection,
-  Batcher,
-  getObjectDiff,
-  logger
-} from '../lib/Utility';
+import { getDocumentsInCollection, Batcher } from '../lib/Utility';
+import { createPostAtom } from '../lib/Atoms';
 
 // onWritePost({before: {}, after: {createdAt: null, description: "The question at the heart of this gamble is: are people really going to hang out in virtual reality?", image: "https://wp-assets.futurism.com/2018/06/realestate-600x315.png", medium: "article", publisher: {logo: "", name: "Futurism"}, title: "People are paying insane amounts of real money for 'virtual real estate'", updatedAt: null, url: 'https://futurism.com/virtual-real-estate/amp/'}}, {params: {postId: "yDZVi7G4U5yThribDE7G"}})
 // onWritePost({before: {createdAt: null, description: "The question at the heart of this gamble is: are people really going to hang out in virtual reality?", image: "https://wp-assets.futurism.com/2018/06/realestate-600x315.png", medium: "article", publisher: {logo: "", name: "Futurism"}, title: "People are paying insane amounts of real money for 'virtual real estate'", updatedAt: null, url: 'https://futurism.com/virtual-real-estate/amp/'}, after: {createdAt: null, description: "The question at the heart of this gamble is: are people really going to hang out in virtual reality?", image: "https://wp-assets.futurism.com/2018/06/realestate-600x315.png", medium: "article", publisher: {logo: "", name: "Futurism"}, sharesCount: 10, title: "People are paying insane amounts of real money for 'virtual real estate'", updatedAt: null, url: 'https://futurism.com/virtual-real-estate/amp/'}}, {params: {postId: "yDZVi7G4U5yThribDE7G"}})
 export const _onWritePost = async (db: any, change: any, context: any) => {
   // posts/{postId}
 
-  logger(change);
-
   // write Post updates to matching Posts in all user_posts
   const postId = context.params.postId;
-  const difference = getObjectDiff(
-    change.after.data() || {},
-    change.before.data() || {}
-  );
+  // const difference = getObjectDiff(
+  //   change.after.data() || {},
+  //   change.before.data() || {}
+  // );
+  // const payload = _.pick(change.after.data(), difference);
 
-  const payload = _.pick(change.after.data(), difference);
+  const postAtom = createPostAtom(change.after.data(), postId);
 
   // get all matching Posts in users_posts
   const users_posts = await getDocumentsInCollection(
@@ -33,7 +28,7 @@ export const _onWritePost = async (db: any, change: any, context: any) => {
 
   for (const user_postId in users_posts) {
     if (users_posts.hasOwnProperty(user_postId)) {
-      batcher.set(db.doc(users_posts[user_postId].ref), payload, {
+      batcher.set(db.doc(users_posts[user_postId].ref), postAtom, {
         merge: true
       });
     }
